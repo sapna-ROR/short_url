cb981d5e6107fb550cfd6e309d37f8808193df23
sapnapdev <sapnapdev883@gmail.com>
2024-01-09 14:23:31 +0530

    Add Logic

--- app/controllers/short_links_controller.rb
+++ app/controllers/short_links_controller.rb
@@ -1,3 +1,39 @@
-class ShortLinksController < ApplicationController
+# frozen_string_literal: true
 
+class ShortLinksController < ApplicationController
+  before_action :set_short_url, only: :show
+
+  def create
+    short_link = ShortLink.find_or_initialize_by(url: short_link_params[:url]).tap do |link|
+      link.custom_key = short_link_params[:custom_key]
+      link.expires_in_days = short_link_params[:expires_in_days]
+
+      link.save!
+    end
+
+    json_response({
+                    message: 'success',
+                    data: { short_url: short_link_url(short_link.short_url_identifer),
+                            expires_on: short_link.expires_on }
+                  }, :created)
+  end
+
+  def show
+    @short_url.increment_click_count!
+
+    json_response({
+                    message: 'success',
+                    data: { redirect_to: @short_url.url, metrices: { click_count: @short_url.click_count } }
+                  })
+  end
+
+  private
+
+  def set_short_url
+    @short_url = ShortLink.find_by_uuid_or_custom_key(params[:id])
+  end
+
+  def short_link_params
+    params.require(:short_link).permit(:url, :expires_in_days, :custom_key)
+  end
 end
--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,3 +1,33 @@
-class ShortLink < ApplicationRecord
+# frozen_string_literal: true
 
+class ShortLink < ApplicationRecord
+  DEFAULT_EXPIRY_DAYS = 30
+
+  attr_accessor :expires_in_days
+
+  validates_presence_of :url
+  validates_uniqueness_of :uuid, :custom_key
+
+  before_validation do
+    self.uuid = SecureRandom.hex(3)
+    self.expires_on = Time.current + expiry_days
+  end
+
+  def self.find_by_uuid_or_custom_key(unique_id)
+    find_by!('uuid = ? OR custom_key = ?', unique_id, unique_id)
+  end
+
+  def short_url_identifer
+    custom_key.present? ? custom_key : uuid
+  end
+
+  def increment_click_count!
+    update(click_count: click_count + 1)
+  end
+
+  private
+
+  def expiry_days
+    (expires_in_days ? expires_in_days.to_i : DEFAULT_EXPIRY_DAYS).days
+  end
 end
